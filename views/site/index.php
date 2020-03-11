2c73ade87e6855940cea6d13d41311a6349c19de
Oscar Megía López <pfgytk@gmail.com>
2020-03-11 12:22:09 +0100

    Añadidas las opciónes:
    - Cuando pulsamos sobre el nombre nos abrira una nueva pestaña con el enlace
      al que apunta el marcador.
    - Cuando pulsamos sobre la descripción nos mostrará una ventana modal
      con la descripción larga con un botón para visita que nos irá al
      enlace que apunta el marcador.
    - Añadido script con backup de la base de datos.

--- views/site/index.php
+++ views/site/index.php
@@ -6,6 +6,28 @@ $this->title = 'Página principal';
 use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\helpers\Url;
+
+$script = "$('#myModal').on('show.bs.modal', function (event) {
+  var link = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = link.data('descripcion_larga') // Extract info from data-* attributes
+  var url = link.data('url') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  var btnVisitar =$('#btnVisitar')
+  modal.find('.modal-body').html(recipient)
+  btnVisitar.attr('data-url', url);
+})";
+
+$this->registerJs($script);
+
+$script2 = "$('#btnVisitar').click(function() {
+  var btnVisitar = $(this)
+  // Funciona ir a la página anterior
+  window.location.href = btnVisitar.attr('data-url');
+})";
+
+$this->registerJs($script2);
 ?>
 <div style="text-align: center;">
     <ul class="nav nav-pills" style="display: inline-block;">
@@ -27,7 +49,30 @@ use yii\helpers\Url;
         ],
         [
             'label' => 'Descripción',
-            'value' => 'descripcion_corta',
+            'format' => 'raw',
+            'value' => function($data) {
+                return Html::a($data->descripcion_corta, '#',
+                        ["data-toggle" => "modal", "data-target" => "#myModal",  "data-descripcion_larga" => "$data->descripcion_larga", "data-url" => "$data->url"]);
+            }
         ],
     ]
 ]) ?>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h3 class="modal-title" id="myModalLabel">Descripcion</h3>
+      </div>
+      <div class="modal-body">
+          
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button id="btnVisitar" type="button" class="btn btn-default btn-primary" data-dismiss="modal" data-url="">Visitar</button>
+      </div>
+    </div>
+  </div>
+</div>
